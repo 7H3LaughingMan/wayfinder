81d8c4a1bf71094c518729335eade8be9cd64562
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-08-07 01:10:56 -0500

    Made some changes to the build process

--- build.js
+++ build.js
@@ -1,3 +1,8 @@
-import { compilePack } from "@foundryvtt/foundryvtt-cli";
+import { compilePack, TYPE_COLLECTION_MAP } from "./lib/package.mjs";
 
-await compilePack("packs/src/wayfinder-macros", "packs/wayfinder-macros");
+async function transformEntry(entry) {
+    entry._key = `!${TYPE_COLLECTION_MAP[entry._type]}!${entry._id}`;
+    delete entry._type;
+}
+
+await compilePack("packs/src/wayfinder-macros", "packs/wayfinder-macros", { transformEntry });
